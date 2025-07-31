32d278851f39748c15a0823ded9dac253db44ceb
Victor-bit-tech994 <vekenta3@gmail.com>
2025-07-31 15:28:44 +0100

    Create train_model.py

---
+++ train_model.py
@@ -0,0 +1,5 @@
+import joblib
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import numpy as np
